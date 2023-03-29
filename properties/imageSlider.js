a78262e6506a80fb7b08572650c548e6bc2e1653
dsaffer <56101196+dsaffer@users.noreply.github.com>
2023-03-29 17:25:15 -0400

    2410 updated, switched to for sale

---
+++ properties/imageSlider.js
@@ -0,0 +1,17 @@
+function showSlider(image) {
+    // Get all the img elements on the page
+    const images = document.querySelectorAll('img');
+  
+    // Extract the src attributes and create an array of image URLs
+    const imageUrls = Array.from(images).map(img => ({ src: img.src }));
+  
+    // Find the index of the clicked image in the array
+    const index = Array.from(images).findIndex(img => img.src === image.src);
+    // Open the fancybox with all the images starting from the clicked image
+    $.fancybox.open(imageUrls, { index: index });
+}
+
+const images = document.querySelectorAll('img');
+images.forEach(image => {
+  image.addEventListener('click', showSlider(this));
+});
