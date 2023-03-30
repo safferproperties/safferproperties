f603497a0c419a6cb0641adebbb4e95f1e3f73c6
davidsaffer <davidsaffer@vt.edu>
2023-03-30 13:02:07 -0400

    updated all properties

--- properties/imageSlider.js
+++ properties/imageSlider.js
@@ -1,14 +1,28 @@
 function showSlider(image) {
-    // Get all the img elements on the page
-    const images = document.querySelectorAll('img');
-    // Extract the src attributes and create an array of image URLs
-    const imageUrls = Array.from(images).map(img => ({ src: img.src }));
+  // Get all the img elements on the page
+  const images = document.querySelectorAll('img');
+  // Extract the src attributes and create an array of image URLs
+  let imageUrls = Array.from(images).map(img => ({ src: img.src }));
   
-    // Find the index of the clicked image in the array
-    const index = Array.from(images).findIndex(img => img.src === image.src);
-    // Open the fancybox with all the images starting from the clicked image
-    $.fancybox.open(imageUrls, { index: index });
+  // Sort the imageUrls array by file name in ascending order
+  imageUrls.sort(function(a, b) {
+    const fileNameA = a.src.split('/')[5];
+    const numericPartA = fileNameA.replace(/image|\.jpg/g, "");
+    const numberA = parseInt(numericPartA);
+    const fileNameB = b.src.split('/')[5];
+    const numericPartB = fileNameB.replace(/image|\.jpg/g, "");
+    const numberB = parseInt(numericPartB);
+    // Compare the file names in ascending order
+    return numberA - numberB;
+  });
+  console.log(imageUrls)
+  // Find the index of the clicked image in the sorted array
+  const index = imageUrls.findIndex(img => img.src === image.src);
+  // Open the fancybox with all the images starting from the clicked image
+  $.fancybox.open(imageUrls, { index: index });
 }
+
+
 
 const images = document.querySelectorAll('img');
 images.forEach(image => {
